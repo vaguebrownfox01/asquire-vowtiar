5c8a7645f25a36384e1b287c44647539533e7f6a
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-12-07 12:05:08 +0530

    push fb config

---
+++ src/functions/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyCSNfJVcgfQrq9oVDzzclKbgFYloVxwPK0",
+	authDomain: "asquire-mox.firebaseapp.com",
+	projectId: "asquire-mox",
+	storageBucket: "asquire-mox.appspot.com",
+	messagingSenderId: "1057786994828",
+	appId: "1:1057786994828:web:0b7c36c7d01b93d72b9ba9",
+	measurementId: "G-RR2BVRP73M",
+};
