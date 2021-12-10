6e22c2b702538dc60d0039632d7a9ccfc569cb92
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-12-10 18:22:26 +0530

    bump version - code: dsp

--- src/functions/storage.js
+++ src/functions/storage.js
@@ -5,7 +5,8 @@ const storageRef = stor.ref();
 
 export const firebaseUserAudio = (user, audio) => {
 	const userAudioRef = storageRef.child(AUDIO_DATA_FOLDER).child(user.userId);
-	const filename = `webapp-${PROJECT_ID}_${user.userId}_${user.stimTag}_${user.completed}.wav`;
+	const score = `${Math.round(user.score)}`.replace(".");
+	const filename = `webapp-${PROJECT_ID}_${user.userId}_${user.stimTag}_${user.completed}_${score}.wav`;
 
 	userAudioRef
 		.child(filename)
