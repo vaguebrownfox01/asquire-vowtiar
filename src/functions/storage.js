6411bd6c080010f8f293f65169bbe4bc7278baee
vaguebrownfox <foxjournal.nsfw@gmail.com>
2022-05-10 14:10:38 +0530

    ver code in filename

--- src/functions/storage.js
+++ src/functions/storage.js
@@ -1,12 +1,12 @@
 import { stor } from "./firebase";
-import { AUDIO_DATA_FOLDER, PROJECT_ID } from "./firebaseSetup";
+import { AUDIO_DATA_FOLDER, PROJECT_ID, VERSION } from "./firebaseSetup";
 
 const storageRef = stor.ref();
 
 export const firebaseUserAudio = (user, audio) => {
 	const userAudioRef = storageRef.child(AUDIO_DATA_FOLDER).child(user.userId);
 	const score = `${Math.round(user.score)}`.replace(".");
-	const filename = `webapp-${PROJECT_ID}_${user.userId}_${user.stimTag}_${user.completed}_${score}.wav`;
+	const filename = `webapp-${PROJECT_ID}-${VERSION}_${user.userId}_${user.stimTag}_${user.completed}_${score}.wav`;
 
 	userAudioRef
 		.child(filename)
